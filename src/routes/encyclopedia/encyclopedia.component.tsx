296ffbd9aa0c34ff26ca30320b814a0cc9cd562a
MajaZubko <majamzubko@gmail.com>
2021-04-05 17:45:18 +0200

    Add information in cards

--- src/routes/encyclopedia/encyclopedia.component.tsx
+++ src/routes/encyclopedia/encyclopedia.component.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Container } from './encyclopedia.styles';
+import { Card, CardContent } from '@material-ui/core';
+import { Container, LatinName, NeedsContainer, PlantName, WaterNeed } from './encyclopedia.styles';
 import { useGetPlants } from './encyclopedia.queries';
+import { EncyclopediaPlant } from './encyclopedia.types';
 
 export const Encyclopedia = () => {
-  const { data, isFetching } = useGetPlants();
+  const { data, isFetching }: { data?: EncyclopediaPlant[]; isFetching: boolean } = useGetPlants();
   console.log(data);
 
-  return <Container>{!!data && data.map((plant) => <div key={plant.id}>{plant.name}</div>)}</Container>;
+  return (
+    <Container>
+      {!!data &&
+        data.map((plant) => (
+          <Card>
+            <CardContent key={plant.id}>
+              <PlantName>{plant.name}</PlantName>
+              <LatinName>{plant.latinName}</LatinName>
+              <NeedsContainer>
+                <WaterNeed>Water: every {plant.waterNeeds} days </WaterNeed>
+                <div>Sun: {plant.sunNeeds}</div>
+              </NeedsContainer>
+            </CardContent>
+          </Card>
+        ))}
+    </Container>
+  );
 };
--- src/routes/encyclopedia/encyclopedia.styles.ts
+++ src/routes/encyclopedia/encyclopedia.styles.ts
@@ -5,3 +5,19 @@ export const Container = styled.div`
   flex-direction: column;
   margin: 0 24px;
 `;
+
+export const PlantName = styled.h3``;
+
+export const LatinName = styled.p`
+  font-style: italic;
+`;
+
+export const NeedsContainer = styled.div`
+  display: flex;
+`;
+
+export const WaterNeed = styled.div`
+  margin-right: 16px;
+`;
+
+// export const Dimension
