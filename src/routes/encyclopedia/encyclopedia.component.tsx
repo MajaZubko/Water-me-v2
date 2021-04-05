ae7b4c91d02683ee85be31c49552ad04fbf91406
MajaZubko <majamzubko@gmail.com>
2021-04-05 17:35:28 +0200

    Add fetching plants on load

--- src/routes/encyclopedia/encyclopedia.component.tsx
+++ src/routes/encyclopedia/encyclopedia.component.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Container } from './encyclopedia.styles';
+import { useGetPlants } from './encyclopedia.queries';
 
 export const Encyclopedia = () => {
-  return <Container>Encyclopedia</Container>;
+  const { data, isFetching } = useGetPlants();
+  console.log(data);
+
+  return <Container>{!!data && data.map((plant) => <div key={plant.id}>{plant.name}</div>)}</Container>;
 };
