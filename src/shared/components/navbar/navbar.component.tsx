698b188b430d1420de9b564c802d337502988020
MajaZubko <majamzubko@gmail.com>
2021-05-17 11:13:51 +0200

    Fix mobile view of encyclopedia and navbar

--- src/shared/components/navbar/navbar.component.tsx
+++ src/shared/components/navbar/navbar.component.tsx
@@ -4,11 +4,16 @@ import { useSelector } from 'react-redux';
 import { H1 } from '../../../theme/typography';
 import { localesSelectors } from '../../../modules/locales';
 import { ROUTES } from '../../../routes/app.constants';
+import { getActiveBreakpoint } from '../../../theme/media';
 import { NavbarLink } from '../../navbarLink';
-import { Container, Heading, Logo, LinksSection } from './navbar.styles';
+import { Container, Heading, Logo, LinksSection, MenuIcon } from './navbar.styles';
 
 export const Navbar = () => {
+  const breakpoint = getActiveBreakpoint();
+  const isMobile = breakpoint === 'mobile';
+
   const [activeRoute, setActiveRoute] = useState(window.location.pathname);
+  const [isMenuOpened, setIsMenuOpened] = useState(!isMobile);
 
   const language = useSelector(localesSelectors.selectLocalesLanguage);
 
@@ -24,6 +29,9 @@ export const Navbar = () => {
     if (window.location.pathname !== activeRoute) {
       setActiveRoute(window.location.pathname);
     }
+    if (isMobile) {
+      setIsMenuOpened(false);
+    }
   };
 
   const isActiveRoute = (route: string): boolean => activeRoute === route;
@@ -35,25 +43,30 @@ export const Navbar = () => {
           <FormattedMessage id="pageTitle" defaultMessage="Water me" description="App / Page title" />
         </H1>
         <Logo />
+        {isMobile && <MenuIcon onClick={() => setIsMenuOpened(!isMenuOpened)} />}
       </Heading>
-      <LinksSection>
-        <NavbarLink to={PATHS.calendar} onClick={handleLinkClick} active={isActiveRoute(PATHS.calendar)}>
-          <FormattedMessage
-            id="wateringCalendarLink"
-            defaultMessage="Watering calendar"
-            description="Watering calendar / Link"
-          />
-        </NavbarLink>
-        <NavbarLink to={PATHS.allPlants} onClick={handleLinkClick} active={isActiveRoute(PATHS.allPlants)}>
-          <FormattedMessage id="allPlantsLink" defaultMessage="List of plants" description="All plants / Link" />
-        </NavbarLink>
-        <NavbarLink to={PATHS.encyclopedia} onClick={handleLinkClick} active={isActiveRoute(PATHS.encyclopedia)}>
-          <FormattedMessage
-            id="encyclopediaLink"
-            defaultMessage="Plant encyclopedia"
-            description="Plant encyclopedia / Link"
-          />
-        </NavbarLink>
+      <LinksSection isMenuOpened={isMenuOpened}>
+        {isMenuOpened && (
+          <>
+            <NavbarLink to={PATHS.calendar} onClick={handleLinkClick} active={isActiveRoute(PATHS.calendar)}>
+              <FormattedMessage
+                id="wateringCalendarLink"
+                defaultMessage="Watering calendar"
+                description="Watering calendar / Link"
+              />
+            </NavbarLink>
+            <NavbarLink to={PATHS.allPlants} onClick={handleLinkClick} active={isActiveRoute(PATHS.allPlants)}>
+              <FormattedMessage id="allPlantsLink" defaultMessage="List of plants" description="All plants / Link" />
+            </NavbarLink>
+            <NavbarLink to={PATHS.encyclopedia} onClick={handleLinkClick} active={isActiveRoute(PATHS.encyclopedia)}>
+              <FormattedMessage
+                id="encyclopediaLink"
+                defaultMessage="Plant encyclopedia"
+                description="Plant encyclopedia / Link"
+              />
+            </NavbarLink>
+          </>
+        )}
       </LinksSection>
     </Container>
   );
